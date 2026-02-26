ed4bf416ea324e4a9d824d1c5d1d18cabf68ce19
mentolbreak1 <98565793+mentolbreak1@users.noreply.github.com>
2026-02-26 18:16:55 +0300

    Update tree_node.py

--- bst-project/src/tree_node.py
+++ bst-project/src/tree_node.py
@@ -1,1 +1,20 @@
-
+class TreeNode:
+    """Класс узла бинарного дерева"""
+    
+    def __init__(self, key, value):
+        """
+        Инициализация узла дерева
+        
+        Args:
+            key: Ключ узла
+            value: Значение узла
+        """
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1  # высота для удобства вычислений
+        
+    def __repr__(self):
+        """Строковое представление узла"""
+        return f"TreeNode({self.key}: {self.value})"
